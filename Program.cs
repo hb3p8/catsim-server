5f1ab5613ebce2a0d8731046b2d59c62d64b33f7
duv <duv@gmail.com>
2016-02-13 14:56:49 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Example
+{
+    public class CatServer : WebSocketBehavior
+    {
+        protected override void OnOpen()
+        {
+            Console.WriteLine("New user!");
+
+            foreach (string id in Sessions.IDs)
+            {
+                Console.WriteLine(id);
+                Sessions.SendTo("Asd", id);
+            }
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine("Message received: " + e.Data);
+            var msg = e.Data == "HELLO"
+                      ? "Nya"
+                      : "Pff";
+
+            Send(msg);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer("ws://127.0.0.1");
+            wssv.AddWebSocketService<CatServer>("/cat");
+            wssv.Start();
+            Console.WriteLine("Server started");
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+}
